79896f6cf4bc1c88227ccbc99ad1c3ddf7895fec
sris1582 <148256801+sris1582@users.noreply.github.com>
2026-03-13 23:36:21 +0530

    Update default S3 bucket name for Terraform

--- terraform/terraform_backend/variable.tf
+++ terraform/terraform_backend/variable.tf
@@ -9,7 +9,7 @@ variable "aws_region" {
 variable "aws_s3_bucket_name" {
   description = "The name of the S3 bucket for storing Terraform state files securely."
   type        = string
-  default     = "github-actions-bucket-new"
+  default     = "github-actions-bucket-new15"
 }
 
 variable "aws_dynamodb_table_name" {
